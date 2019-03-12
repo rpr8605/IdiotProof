2c52627d75a68d4ca64727a9b551836da517000a
Robert O <romohund@gmail.com>
2019-03-11 20:20:53 -0500

    SQL database and server.js file created

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,12 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+create database items_db;
+use items_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+create table items (
+id int auto_increment not null,
+item_name varchar(255) not null,
+category varchar(255),
+expiration_date date not null,
+notes text,
+warranty_valid boolean default true,
+primary key (id)
+);
