ac8b9fa4eff5c679c30c506a6bad8b44735fdf2f
Robert Martin <robert4044@gmail.com>
2019-03-11 20:42:38 -0500

    adding original connection and itemsjs

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+let Sequelize = require("sequelize");
+
+let sequelize = new Sequelize("items_db", "root", "", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+module.exports = sequelize;
