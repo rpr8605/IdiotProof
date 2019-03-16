d5553339be23fbe817eebcdab47733c43269979e
Robert Martin <robert4044@gmail.com>
2019-03-16 10:45:40 -0500

    changed routing parameters

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-const accountSid = 'ACb3cd2a2b23b182e38a3cafa530af2c63';
-const authToken = '93bbc9a970c9f088fbb437c91fb50766';
-const client = require('twilio')(accountSid, authToken);
+
+let routes = require("./routes/apiRoutes");
+
+
+// const accountSid = 'ACb3cd2a2b23b182e38a3cafa530af2c63';
+// const authToken = '93bbc9a970c9f088fbb437c91fb50766';
+// const client = require('twilio')(accountSid, authToken);
 
 
 //+16602102135
@@ -17,16 +21,18 @@ const client = require('twilio')(accountSid, authToken);
 
 var app = express();
 
+
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(routes);
+
+//require("./routes/apiRoutes")(app);
 
 
 
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
